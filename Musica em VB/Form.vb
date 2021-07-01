86697969b76db70f09f42c864ea462fa67d469b9
Invazing <invazing@hotmail.com>
2021-07-01 10:09:30 -0300

    Adicionar .gitignore e .gitattributes.

---
+++ Musica em VB/Form.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        My.Computer.Audio.Play(My.Resources.KeyGenMusic, AudioPlayMode.Background)
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        My.Computer.Audio.Stop()
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        My.Computer.Audio.Play(My.Resources.KeyGenMusic, AudioPlayMode.BackgroundLoop)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        My.Computer.Audio.PlaySystemSound(System.Media.SystemSounds.Hand)
+    End Sub
+End Class
---
+++ Musica em VB/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' As informações gerais sobre um assembly são controladas por
+' conjunto de atributos. Altere estes valores de atributo para modificar as informações
+' associada a um assembly.
+
+' Revise os valores dos atributos do assembly
+
+<Assembly: AssemblyTitle("Musica em VB")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Musica em VB")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+<Assembly: Guid("89410df3-3f15-448b-9492-d04961c03acd")>
+
+' As informações da versão de um assembly consistem nos quatro valores a seguir:
+'
+'      Versão Principal
+'      Versão Secundária 
+'      Número da Versão
+'      Revisão
+'
+' É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+' usando o "*" como mostrado abaixo:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
